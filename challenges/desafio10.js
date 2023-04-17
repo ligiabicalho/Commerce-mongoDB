f017ab40465872f6549d054a6f930ceb733df74a
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-17 19:44:56 -0300

    feat: Retorne o nome de todos os lanches que tenham o percentual de proteínas maior ou igual a 30 e menor ou igual a 40

---
+++ challenges/desafio10.js
@@ -0,0 +1,16 @@
+db.produtos.find(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+          tipo: "proteínas",
+          percentual: {
+            $gte: 30, 
+            $lte: 40 },
+      },
+    },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
