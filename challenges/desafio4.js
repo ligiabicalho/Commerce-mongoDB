02468f52e09d0dba80fc87fba870b05191dd8bb5
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-17 18:52:42 -0300

    feat: Retorne os lanches que tiveram vendas maiores que 50 e menores que 100, mostrando apenas o nome e a quantidade de lanches vendidos em ordem crescente

---
+++ challenges/desafio4.js
@@ -0,0 +1,15 @@
+db.produtos.find(
+  {
+    vendidos: { 
+      $gt: 50, 
+      $lt: 100, 
+    },
+  },
+  { 
+    _id: 0,
+    nome: 1, 
+    vendidos: 1, 
+  },
+).sort({
+    vendidos: 1,
+  });
