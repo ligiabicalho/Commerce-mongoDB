e1558572cf06e560418a550753f3b2ac99bf3e9e
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-17 19:42:38 -0300

    feat: Retorne o nome de todos os lanches que possuam calorias abaixo de 500

---
+++ challenges/desafio9.js
@@ -0,0 +1,14 @@
+db.produtos.find(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+          tipo: "calorias",
+          quantidade: { $lt: 500 },
+      },
+    },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
