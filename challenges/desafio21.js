9080b2fc050f017a396c44de4ad15bf9b1acb581
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-18 00:22:50 -0300

    feat: Remova o último ingrediente do sanduíche Cheddar McMelt

---
+++ challenges/desafio21.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  { nome: "Cheddar McMelt" },
+  { $pop: { ingredientes: 1 } },
+);
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  },
+);
