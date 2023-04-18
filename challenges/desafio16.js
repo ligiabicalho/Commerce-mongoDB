f6753fac50ea7f197b9939009516ba4da6e18205
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-17 22:02:43 -0300

    feat: Adicione o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac e retorne o nome de todos os docs q possui o campo ultimaModificacao

---
+++ challenges/desafio16.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: true } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
