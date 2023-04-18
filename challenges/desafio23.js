4f8e38ac5f18c15840061bbb3f8a0d4bf64d03ca
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-18 11:38:00 -0300

    feat: Insira os valores combo e tasty no array tags de todos os sanduíches e aproveite para deixar os valores em ordem alfabética ascendente (A a Z)

---
+++ challenges/desafio23.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany(
+  {},
+  { $push: { 
+      tags: { 
+        $each: ["tasty", "combo"],
+        $sort: 1,
+      }, 
+    },
+  },
+);
+db.produtos.find(
+  { },
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1,
+  },
+);
